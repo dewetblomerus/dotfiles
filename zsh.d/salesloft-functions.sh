f1efe44a37367ae738f2d66b2753bc5982a8eae1
De Wet Blomerus <dewet@blomerus.org>
2020-11-13 11:52:14 -0500

    helpful logging

--- zsh.d/salesloft-functions.sh
+++ zsh.d/salesloft-functions.sh
@@ -16,25 +16,34 @@ function s() {
 function b() {
   if [[ $PWD/ = */src/sidecar/* ]]; then
     cd ~/src/sidecar
+    echo "Setting package.json to production build"
     git checkout package.json
     sed -i '' 's/\"build\": \"NODE_OPTIONS/\"build\": \"NODE_ENV=production NODE_OPTIONS/g' ~/src/sidecar/package.json
+    echo "Running yarn run build"
     yarn run build
     say done
   elif [[ $PWD/ = */src/rhythm/* ]]; then
     cd ~/src/rhythm
+    echo "Deleting everything that is not committed to Git"
     git clean -xdf .
+    echo "running npm install"
     npm install
+    echo "running rush update"
     rush update
+    echo "running rush rebuild"
     rush rebuild
     say done
   elif [[ $PWD/ = */src/rhapsody/* ]]; then
     cd ~/src/rhapsody
+    echo "Removing node_modules and build directories"
     mkdir ../.tmp_to_remove
     mv node_modules ../.tmp_to_remove
     mv build ../.tmp_to_remove
     rm -rf ../.tmp_to_remove &
+    echo "Running npm install"
     npm install
     say done
+    echo "Running npm start"
     npm start
   else
     echo 'This build script currently only works for Rhapsody, Rhythm  and Sidecar'
