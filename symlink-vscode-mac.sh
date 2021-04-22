8febc34ec63fb7bbefc531507888ab2301397ad5
De Wet Blomerus <dewet@blomerus.org>
2021-04-22 07:05:52 -0400

    properly handle vscode settings

--- symlink-vscode-mac.sh
+++ symlink-vscode-mac.sh
@@ -1,3 +1,6 @@
-ln -s ~/dotfiles/vscode.settings.json ~/Library/Application\ Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 rm -rf ~/Library/Application\ Support/Code/User/snippets
 ln -s ~/dotfiles/vscode/user_snippets ~/Library/Application\ Support/Code/User/snippets
