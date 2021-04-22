2d9bf170e2407fe03ad6b14367ed2b74971275f2
De Wet Blomerus <dewet@blomerus.org>
2021-04-22 06:55:43 -0400

    save vscode snippets

--- symlink-vscode-mac.sh
+++ symlink-vscode-mac.sh
@@ -1,1 +1,3 @@
 ln -s ~/dotfiles/vscode.settings.json ~/Library/Application\ Support/Code/User/settings.json
+rm -rf ~/Library/Application\ Support/Code/User/snippets
+ln -s ~/dotfiles/vscode/user_snippets ~/Library/Application\ Support/Code/User/snippets
