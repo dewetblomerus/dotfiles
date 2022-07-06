00cec7b29a1cc6af1084d53bcbf40a64833cd377
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-07-06 09:36:20 -0400

    keep going when file missing 🏎

--- symlink-vscode-mac.sh
+++ symlink-vscode-mac.sh
@@ -1,6 +1,6 @@
-rm ~/Library/Application\ Support/Code/User/settings.json
+rm -f ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-rm ~/Library/Application\ Support/Code/User/keybindings.json
+rm -f ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 rm -rf ~/Library/Application\ Support/Code/User/snippets
 ln -s ~/dotfiles/vscode/user_snippets ~/Library/Application\ Support/Code/User/snippets
