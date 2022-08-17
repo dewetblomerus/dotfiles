23d3d21428f46d9ed66c9bf7316f29028241db5a
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-08-17 16:49:28 -0400

    delete stale stuff

--- homefoldersymlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FOLDERS=(bin code dewetblomerus.github.io Documents Downloads Dropbox Music Pictures Public Templates Videos VirtualBox-VMs);
-SSDPATH=/media/dewet/ssd
-for i in ${FOLDERS[@]}
-do
-  rm -f ~/$i
-  ln -s $SSDPATH/$i ~/$i
-done
